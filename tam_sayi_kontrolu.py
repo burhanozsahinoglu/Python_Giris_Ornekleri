23933c9bc2f40843606c375b80fcc7475b6d29f0
burhanozsahinoglu <45271999+burhanozsahinoglu@users.noreply.github.com>
2019-08-05 19:13:13 +0300

    Create tam_sayi_kontrolu.py

---
+++ tam_sayi_kontrolu.py
@@ -0,0 +1,22 @@
+# Yontem 1
+
+print("Tam Sayi Kontrolu 1")			 	
+sayi = eval(input("Sayi giriniz:"))			
+print(sayi)									
+if sayi == int(sayi):						
+	print("Tamsayi...")						
+else:										
+	print("Tamsayi Degil!")	
+  
+  
+  
+ # Yontem 2
+ 
+ 
+print("Tam Sayi Kontrolu 2")				
+sayi = eval(input("Sayi giriniz:"))			
+print(sayi)									
+if sayi%1 == 0:								
+	print("Tamsayi...")						
+else:										
+	print("Tamsayi Degil!")
